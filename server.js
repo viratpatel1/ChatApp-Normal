ce1b8357f66c6eeb1e933dd76563a295b83017d8
viratpatel1 <viratpatel993@gmail.com>
2023-03-31 00:06:12 +0530

    DOne

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
-const { addUsers, getUsers, removeUser, getUserInRoom } = require("./Componends/users");
+const {
+  addUsers,
+  getUsers,
+  removeUser,
+  getUserInRoom,
+} = require("./Componends/users");
 const router = require("./Componends/router");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
-
-const app = express()
+const app = express();
 app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors:
-    {
-        origins: "http://localhost:3000/",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origins: "http://chatsapps-1.netlify.com/",
+    // origins: "http://localhost:3000/",
+    methods: ["GET", "POST"],
+  },
 });
-
 
 // var server = require('http').Server(app);
 // var io = require('socket.io')(server, {
@@ -30,51 +33,59 @@ const io = new Server(httpServer, {
 
 const PORT = process.env.PORT || 5000;
 
-io.on("connection", (socket) =>
-{
-    console.log("User Connected");
+io.on("connection", (socket) => {
+  console.log("User Connected");
 
-    socket.on('join', ({ name, room }, callback) =>
-    {
-        // console.log("server ", name, room);
-        const { error, user } = addUsers({ id: socket.id, name, room });
+  socket.on("join", ({ name, room }, callback) => {
+    // console.log("server ", name, room);
+    const { error, user } = addUsers({ id: socket.id, name, room });
 
-        if (error) return callback(error);
+    if (error) return callback(error);
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
-
-        //send msg to everyone beside that specific user
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-        socket.join(user.room);
-
-        io.to(user.room).emit('roomData', { room: user.room, users: getUserInRoom(user.room) });
-
-        callback();
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, Welcome to the room ${user.room}`,
     });
 
-    socket.on('sendMessage', (message, callback) =>
-    {
-        const user = getUsers(socket.id);
+    //send msg to everyone beside that specific user
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+    socket.join(user.room);
 
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        io.to(user.room).emit('roomData', {
-            room: user.room, users: getUserInRoom(user.room)
-        });
-        callback();
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUserInRoom(user.room),
     });
 
-    socket.on("disconnect", () =>
-    {
-        const user = removeUser(socket.id);
-        if (user)
-        {
-            io.to(user.room).emit('message', { user: "admin", text: `${user.name} has Left` })
-        }
-        console.log("User Disconnected !!!");
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUsers(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUserInRoom(user.room),
     });
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has Left`,
+      });
+    }
+    console.log("User Disconnected !!!");
+  });
 });
-
 
 app.use(router);
 
-httpServer.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+httpServer.listen(PORT, () =>
+  console.log(`Server has started on port ${PORT}`)
+);
